c9197fb40775ac7e5403ce86552b2b32ddfa522d
Artem Butko <artembutko@Mac.lan>
2025-09-10 12:15:15 -0700

    Remove static file serving from Express server - nginx handles static files

--- server.js
+++ server.js
@@ -761,12 +761,6 @@ app.get('/api/voice/:filename', (req, res) => {
     }
 });
 
-// Static files
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get(/.*/, (_req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 });
